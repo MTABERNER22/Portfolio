9d7372b581f161571f500e2cc3c67a9a2465838d
MTABERNER22 <matiasetaberner16@yahoo.com>
2023-07-15 13:29:53 -0300

    sobre mi

--- src/Componentes/SobreMi.jsx
+++ src/Componentes/SobreMi.jsx
@@ -5,7 +5,7 @@ function SobreMi() {
     return (
         <div className="containerSobreMi" id="sobreMi">
             <p className="tituloSobreMi">Sobre mi</p>
-            <p className="textoSobreMi">Mi nombre es Matías Ezequiel Taberner, tengo 23 años. Oriundo de Merlo, provincia de Buenos Aires, Argentina. Me recibí en el año 2021 de técnico en seguridad e higene en el trabajo, por lo que la corta experiencia laboral que poseo es en ese ámbito. Pero en el año 2022 descubrí el mundo de la programación y quede fascinado. Actualmente me encuentro buscando empleo en materia de Desarrollo Web Full Stack, tanto en lenguajes como Java ,PHP, y JavaScript. Especialmente en librerías de JavaScript, como React.Js, Node,js y Angular.js. Me considero una persona autodidacta, ya que me gusta informarme a diario sobre nuevas formas de programar. Dispuesto a trabajar.</p>
+            <p className="textoSobreMi">Mi nombre es Matías Ezequiel Taberner, tengo 23 años. Oriundo de Merlo, provincia de Buenos Aires, Argentina. Me recibí en el año 2021 de técnico en seguridad e higene en el trabajo, por lo que la corta experiencia laboral que poseo es en ese ámbito. Actualmente me encuentro trabajando como preventor en higiene y seguridad en obras en la empresa Provincia Art. Pero en el año 2022 descubrí el mundo de la programación y quede fascinado. Actualmente me encuentro buscando empleo en materia de Desarrollo Web Full Stack, tanto en lenguajes como Java ,PHP, y JavaScript. Especialmente en librerías de JavaScript, como React.Js, Node,js y Angular.js. Me considero una persona autodidacta, ya que me gusta informarme a diario sobre nuevas formas de programar. Dispuesto a trabajar.</p>
         </div>
     )
 }
